1a6bd1f12b5351d69dc4c7fa321995f31d8175cd
DELL <oliver885@hotmail.com>
2020-04-24 18:30:43 -0500

    Sigo con actualizaciones de CSS

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ session_destroy();
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<!-- <link rel="stylesheet" href="css/style.css"> -->
+	<link rel="stylesheet" href="css/style.css">
 	<title>ALSUR</title>
 </head>
 <body class="fondo">
